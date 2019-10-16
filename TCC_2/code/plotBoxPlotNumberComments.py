cd38f3315c8a9b724564ad3bd0a4f72baf6ac128
claudiaps <clp.sampedro@gmail.com>
2019-10-16 20:53:36 -0300

    função para remoção de ouliers do box plot de comentarios

--- TCC_2/code/plotBoxPlotNumberComments.py
+++ TCC_2/code/plotBoxPlotNumberComments.py
@@ -1,10 +1,10 @@
 import json
 import plotly.graph_objects as go
+from outliers import smirnov_grubbs as grubbs
 
 with open('../data/boxplotNumberComments.json') as f:
     data = json.load(f)
 fig = go.Figure()
-fig.add_trace(go.Box(y=data, name='comments'))
-
+fig.add_trace(go.Box(y=grubbs.test(data, alpha=0.20), name='comments'))
 fig.show()
 
